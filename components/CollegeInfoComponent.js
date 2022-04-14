37323504cd7b5f38b812c8c793bd9cec9da41bdf
Abbey Drake <drakeam1189@gmail.com>
2022-04-14 15:21:05 -0600

    favorite buton

--- components/CollegeInfoComponent.js
+++ components/CollegeInfoComponent.js
@@ -1,19 +1,33 @@
 import React, {Component} from "react";
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
+import { Text, View, StyleSheet } from "react-native";
+import { Card, Icon } from "react-native-elements";
 import { COLLEGES } from "../shared/colleges";
 
-function RenderCollege({ college }) {
+function RenderCollege(props) {
+    const {college} = props;
+  const { navigate } = props;
     if (college) {
         return (
-            <Card
-                featuredTitle={college.name}
-                image={college.image}
-            >
-                <Text style={{ margin: 10 }}>{`Located in: ${college.location}`}</Text>
-                <Text style={{ margin: 10 }}>{`Type of University: ${college.type}`}</Text>
-                <Text style={{ margin: 10 }}>{college.description}</Text>
-            </Card>
+            <View>
+                <Card
+                    featuredTitle={college.name}
+                    image={college.image}>
+                        <Text style={{ margin: 10 }}>{`Located in: ${college.location}`}</Text>
+                        <Text style={{ margin: 10 }}>{`Type of University: ${college.type}`}</Text>
+                        <Text style={{ margin: 10 }}>{college.description}</Text>
+                        <View style={styles.cardRow}>
+                        <Icon
+                            name={props.favorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={() => props.favorite ? 
+                                console.log('Already set as a favorite') : props.markFavorite()}
+                        />
+                        </View>
+                </Card>
+            </View>
         );
     }
     return <View />;
@@ -24,7 +38,12 @@ class CollegeInfo extends Component {
       super(props);
       this.state = {
         colleges: COLLEGES,
+        favorite: false
       };
+    }
+
+    markFavorite() {
+        this.setState({favorite: true});
     }
   
   
@@ -47,10 +66,22 @@ class CollegeInfo extends Component {
          <RenderCollege 
               college={college}
               navigate = {navigate}
+              favorite={this.state.favorite}
+              markFavorite={() => this.markFavorite()}
           />
          
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 30
+    },
+});
 
 export default CollegeInfo;
