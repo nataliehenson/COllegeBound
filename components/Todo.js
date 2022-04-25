60f9470f7ed3d871a5fadaa7ac780195caf29fbc
Natalie Henson <nmhenson13@gmail.com>
2022-04-25 19:13:39 -0400

    added structure to todo list, swiperow

--- components/Todo.js
+++ components/Todo.js
@@ -1,45 +1,104 @@
 import React, { Component } from "react";
-import { View, ScrollView, StyleSheet, Text } from "react-native";
-
-
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { TODOITEMS } from "../shared/todoitems";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoitems: TODOITEMS,
+        };
+    }
+
     render() {
-        return(
-            <>
+        const renderTodoItem = ({ item }) => {
+            return (
+                <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
+                    <View style={styles.completedView}>
+                        <TouchableOpacity style={styles.completedTouchable}>
+                            <Text style={styles.completedText}>Completed</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <ListItem
+                            title={item.name}
+                            subtitle={item.date}
+                            style={styles.todo}
+                        />
+                    </View>
+                </SwipeRow>
+            );
+        };
+
+        return (
+            <View>
                 <View style={styles.header}>
-                    <Text style={styles.name}>To-Do</Text>
+                    <Text style={styles.name}>To-Do List</Text>
                 </View>
-                <ScrollView>
-                    <Text style={styles.body}>Request Transcripts</Text>
-                </ScrollView>
-            </>
-        )
+                <FlatList
+                    data={this.state.todoitems}
+                    renderItem={renderTodoItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </View>
+        );
     }
 }
 
 const styles = StyleSheet.create({
+    completedView: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        flex: 1,
+    },
+    completedTouchable: {
+        backgroundColor: "green",
+        height: "85%",
+        justifyContent: "center",
+    },
+    completedText: {
+        color: "white",
+        fontWeight: "700",
+        textAlign: "center",
+        fontSize: 16,
+        width: 100,
+    },
 
     name: {
-      fontSize: 30,
-      fontWeight: "bold",
-      color: "white",
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
     },
-
+    todo: {
+        backgroundColor: "white",
+        fontSize: 15,
+        color: "black",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+        margin: 10,
+    },
     body: {
         color: "black",
-        fontSize: 20, 
+        fontSize: 20,
         textAlign: "center",
-        paddingTop: 20
+        paddingTop: 20,
     },
-
     header: {
         backgroundColor: "#75896D",
         alignItems: "center",
         justifyContent: "center",
         padding: 30,
-        paddingTop: 60
-
-    }, 
-  });
+        paddingTop: 60,
+    },
+    stackIcon: {
+        marginLeft: 100,
+        color: "black",
+        fontSize: 24,
+    },
+});
 export default Todo;
---
+++ shared/todoitems.js
@@ -0,0 +1,37 @@
+export const TODOITEMS = [
+    {
+        id: 0,
+        name: "Request Transcripts",
+        date: "September",
+    },
+    {
+        id: 1,
+        name: "Request Recommendations from Two Teachers",
+        date: "September",
+    },
+    {
+        id: 2,
+        name: "Complete the FAFSA",
+        date: "October",
+    },
+    {
+        id: 3,
+        name: "Retake the SAT",
+        date: "October",
+    },
+    {
+        id: 4,
+        name: "Complete Early Decision Applications",
+        date: "November",
+    },
+    {
+        id: 5,
+        name: "Apply to Scholarships",
+        date: "December",
+    },
+    {
+        id: 6,
+        name: "Accept a School!",
+        date: "May",
+    },
+];
